2bfc53eb4045e9165429e84d308f710518e01ae3
Mirajoy <31677653+Mirajoy@users.noreply.github.com>
2017-09-10 14:54:13 -0500

    completes assignment

--- 01 hello.py
+++ 01 hello.py
@@ -5,3 +5,4 @@ import sys
 assert sys.version_info >= (3,4), "This script requires at least Python 3.4"
 
 print('Hello, Class!')
+print("My name is Mirajoy Dixon. \n My favorite game is one of the sequals in the Myst franchise (I don't remember which one it was since I haven't played it in a few years). \n I'm concerned that I'll become unmotivated halfway through the class. \n I tend to get excited about books, crocheting, turtles, and games. \n My stackoverflow.com user number is 8566374 and the url to my github is https://github.com/Mirajoy.")
